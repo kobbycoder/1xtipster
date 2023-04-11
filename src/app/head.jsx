648197f1335632b505e016854ba9a6a218df595b
alexanderosman11 <alexanderosman11@gmail.com>
2023-04-11 23:32:09 +0000

    Next js installation

---
+++ src/app/head.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function CustomHead() {
+  return (
+    <Head>
+      <title>Free - Sporty Betting TIps</title>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta content="Free daily betting tips and analysis" name="description" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/favicon.ico"  />
+    </Head>
+  );
+}
