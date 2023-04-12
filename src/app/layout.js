a7b9626366a9af874aad063ce3a06d2e1be77357
alexanderosman11 <alexanderosman11@gmail.com>
2023-04-12 00:03:00 +0000

    header added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,25 @@
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
 export const metadata = {
-  title: 'Free - Daily Betting prediction',
-  description: 'Free betting sporty 1xbet betway predictions',
-}
+  title: "Free - Daily Betting prediction",
+  description: "Free betting sporty 1xbet betway predictions",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        
+        {/* Header */}
+          <Header />
+        {/* Navbar */}
+
+        {/* Hero */}
+
+        {children}
+        
+        </body>
     </html>
-  )
+  );
 }
