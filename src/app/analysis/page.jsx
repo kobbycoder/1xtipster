ef7e4630143a0f323a92446029e57738e40a3694
alexanderosman11 <alexanderosman11@gmail.com>
2023-04-24 01:20:28 +0000

    Analysis component added

--- src/app/analysis/page.jsx
+++ src/app/analysis/page.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import AffiliateAdPlacement from '@/components/AffiliateAdPlacement'
+import AnalysisComponent from '@/components/AnalysisComponent'
 
 export default function page() {
   return (
-    <div>
-      Analysis
+    <div className="flex w-full min-h-screen justify-between">
+      <div className="hidden lg:flex w-2/6">
+        <AffiliateAdPlacement collection="Ads" id="2" />
+      </div>
+
+      <div className="w-full p-6 lg:p-0 lg:pt-6 md:pt-6">
+        <AnalysisComponent />
+      </div>
+
+      <div className="hidden lg:flex w-2/6">
+        <AffiliateAdPlacement collection="Ads" id="2" />
+      </div>
     </div>
   )
 }
---
+++ src/components/AnalysisComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AnalysisComponent() {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
