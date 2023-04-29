307cb004cc56f3cd1577aec0533a8f39620bda45
alexanderosman11 <alexanderosman11@gmail.com>
2023-04-29 11:59:44 +0000

    set images to unoptimized for now

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   'www.stadebriochin.com', 'e7.pngegg.com', 'www.rainbow-sports.com', 'as01.epimg.net', 'static.holoduke.nl', 'www.gudjaunitedfc.com', 'seeklogo.com',
   'static.wikia.nocookie.net', 'sortitoutsi.net', 'www.stickpng.com'
   ],
+  unoptimized: true,
   },
 }
 
