1fade3810940752e2b9f817001915a836f64d9fb
kobbycoder <alexanderosman11@gmail.com>
2024-02-06 06:09:12 +0000

    chore: Remove pro bet api

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,11 @@ import AffiliateAdPlacement from "@/components/AffiliateAdPlacement";
 import Tabs from "@/components/Tabs";
 import { Inter } from "next/font/google";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 // This function can be named anything
-export async function getData() {
-  const res = await fetch(`https://probet.tips/api/predictions?date=26-04-2023`, { cache: 'no-store' });
-  const data = await res.json();
-
-  return data;
-}
 
 export default async function Home() {
-  const data = await getData();
-  //console.log(data);
-
   return (
     <main className="flex w-full min-h-screen justify-between">
       <div className="hidden lg:flex w-2/6">
@@ -24,8 +14,26 @@ export default async function Home() {
       </div>
 
       <div className="w-full py-6">
-        <div className="w-full px-6 lg:px-0 min-h-max">
-        <Tabs />
+        <div className="w-full px-6 lg:px-0 h-full flex flex-col gap-3 justify-center items-center">
+          {/* <Tabs /> */}
+          <a
+            href="/fixtures"
+            className="bg-teal-600 p-10 rounded-lg text-2xl font-bold"
+          >
+            GO TO FIXTURES
+          </a>
+          <a
+            href="/fixtures"
+            className=" bg-cyan-500 p-10 rounded-lg text-2xl font-bold"
+          >
+            GO TO ANALYSIS
+          </a> 
+          <a
+            href="/fixtures"
+            className=" bg-indigo-500 p-10 rounded-lg text-2xl font-bold"
+          >
+            GO TO VIP
+          </a> 
         </div>
       </div>
 
